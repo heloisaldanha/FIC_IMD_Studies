ef2eb6761bef2cc3a62d97eed21e6449b1b0f41a
Heloísa Saldanha <65875963+heloisaldanha@users.noreply.github.com>
2020-10-11 21:41:58 -0300

    Add files via upload

---
+++ Java/Funções/funcoes/Teste.java
@@ -0,0 +1,18 @@
+package funcoes;
+
+public class Teste {
+     public static void main(String[] args) {
+          Main();
+
+    }
+
+    public static void Main(){
+
+        System.out.println("Copa do mundo.");
+
+        Main();
+
+    }
+
+}
+     
