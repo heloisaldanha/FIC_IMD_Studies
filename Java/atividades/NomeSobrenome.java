24438f01769ecd84e479f560abfcfa536d23cbf9
Heloísa Saldanha <65875963+heloisaldanha@users.noreply.github.com>
2020-10-11 21:46:18 -0300

    Update NomeSobrenome.java

--- Java/atividades/NomeSobrenome.java
+++ Java/atividades/NomeSobrenome.java
@@ -3,8 +3,8 @@ package atividades;
 public class NomeSobrenome {
     public static void main(String[] args) {
         String nome, sobrenome;
-        nome = "Heloísa";
-        sobrenome = "Saldanha";
+        nome = "João";
+        sobrenome = "da Silva";
         System.out.format("Meu nome é %s %s\n", nome, sobrenome);
 
     }
